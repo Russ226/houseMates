e1acf5d93add8e21492d46c6325b4710d2db58e6
russ226 <russ2510@gmail.com>
2018-08-29 22:24:05 -0400

    working on user controller

---
+++ src/main/java/com/housemate/controller/user/UserController.java
@@ -0,0 +1,27 @@
+package com.housemate.controller.user;
+
+import com.housemate.models.User;
+import com.housemate.service.user.UserService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity createUser(@RequestBody User user){
+
+
+    }
+}
