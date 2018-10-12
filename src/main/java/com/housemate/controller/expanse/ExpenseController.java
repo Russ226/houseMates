6c95fb67ee9d01d42e030659f1d479e27d56e2fa
russ226 <russ2510@gmail.com>
2018-10-12 17:28:46 -0400

    testing new expenses methods

--- src/main/java/com/housemate/controller/expanse/ExpenseController.java
+++ src/main/java/com/housemate/controller/expanse/ExpenseController.java
@@ -1,13 +1,14 @@
 package com.housemate.controller.expanse;
 
+import com.housemate.models.User;
 import com.housemate.service.Budget.ExpenseService;
 import com.housemate.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/budget")
+@RequestMapping("/expense")
 public class ExpenseController {
 
     @Autowired
@@ -15,6 +16,17 @@ public class ExpenseController {
 
     @Autowired
     ExpenseService expenseService;
+
+    @RequestMapping(value = "/new", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public HttpStatus createUser(@RequestBody User user, @RequestParam(value = "amount", required = false) String amount){
+        if(!userService.login(user.getUsername())) return HttpStatus.CREATED;
+
+        return HttpStatus.valueOf(409);
+
+    }
+
+
 
     
 }
--- src/test/java/com/housemate/test/budget/TestExpense.java
+++ src/test/java/com/housemate/test/budget/TestExpense.java
@@ -108,7 +108,6 @@ public class TestExpense {
 
         assertEquals(10, expenses.size());
 
-
     }
 
 
