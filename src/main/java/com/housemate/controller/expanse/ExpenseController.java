b66c2ea77a99c87fea596c57f63230ca0ddd5839
russ226 <russ2510@gmail.com>
2018-10-13 16:15:49 -0400

    working on Expense controller

--- src/main/java/com/housemate/controller/expanse/ExpenseController.java
+++ src/main/java/com/housemate/controller/expanse/ExpenseController.java
@@ -1,5 +1,7 @@
 package com.housemate.controller.expanse;
 
+import com.google.gson.Gson;
+import com.housemate.jsonhelpers.auth.AuthKey;
 import com.housemate.jsonhelpers.message.ErrorMessage;
 import com.housemate.jsonhelpers.message.SuccessExpenseMessage;
 import com.housemate.models.User;
@@ -10,6 +12,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.time.Month;
+import java.time.format.DateTimeParseException;
+import java.util.Calendar;
 import java.util.Date;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -28,7 +33,7 @@ public class ExpenseController {
 
     @RequestMapping(value = "/new", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public String createUser(@RequestParam(value = "auth_key") String key, @RequestParam(value = "amount") String amount,
+    public String createExpense(@RequestParam(value = "auth_key") String key, @RequestParam(value = "amount") String amount,
                              @RequestParam(value = "name") String name, @RequestParam(value = "date") String date){
         Date parsedDate = null;
         User user = userService.authenticateUser(key);
@@ -49,7 +54,30 @@ public class ExpenseController {
         }catch (NumberFormatException e){
             return new ErrorMessage("Invalid number").getJson();
         }
+    }
 
+    @RequestMapping(value= "/get/expense", method= RequestMethod.GET)
+    public String getExpense(@RequestParam(value = "month", required = false) String month, @RequestParam(value = "year", defaultValue = "4") int year,
+                             @RequestParam(value = "key", required = true) String key){
+        String parsedMonth;
+        User user = userService.authenticateUser(key);
+        if(user == null) return new ErrorMessage("Invalid Key").getJson();
+
+        if(month != null && year != 4){
+            try{
+                parsedMonth = Month.valueOf(month).name();
+                if(year > Calendar.getInstance().get(Calendar.YEAR) || year < 1900){
+                    return new ErrorMessage("Invalid year").getJson();
+                }
+                return new Gson().toJson(expenseService.getByMonthExpense(user, parsedMonth, year));
+            }catch (IllegalArgumentException e){
+                return new ErrorMessage("Invalid Month").getJson();
+            } catch (ParseException e) {
+                return new ErrorMessage("Invalid Month").getJson();
+            }
+        }
+
+        return new Gson().toJson(expenseService.getThisMonthExpense(user));
 
 
 
